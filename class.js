f50bf14af0a6665bfbd9c9b4332dec50e5a7b13b
SohelTanbir <soheltanbir19@gmail.com>
2023-08-13 12:37:47 +0600

    learn class and static method in a class

--- class.js
+++ class.js
@@ -4,9 +4,19 @@ class Student{
         this.name =  name;
         this.id = id;
     }
-
+    // normal method that can access any object 
+    greeting(){
+        console.log("Welcome to ", this.name);
+    }
+    // static method that can ony access by the class itself
+    static printStudentName(){
+        console.log("Student name = ", this.name);
+    }
 }
 
 var student1 = new Student("sohel", 900);
 var student2 = new Student("shakila",1);
-console.log(student1, student2);
+// console.log(student1, student2);
+// console.log(student1.greeting())
+// console.log(student1.printStudentName()) // out will be an error message
+console.log(Student.printStudentName())
