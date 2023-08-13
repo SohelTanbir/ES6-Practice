5f2696d1564cbb9cf278369a1f52d1e9419a4f11
SohelTanbir <soheltanbir19@gmail.com>
2023-08-13 09:40:16 +0600

    learn destructuring

--- destructure.js
+++ destructure.js
@@ -1,9 +1,24 @@
 // use destructur array and object
-const person = {name:'sohelrana',age:20,salary:25,country:'Bangladesh'}
+const person = {
+    name:'sohelrana',
+    age:20,
+    salary:25,
+    country:'Bangladesh',
+    job:{
+        job1:"Developer",
+        job2:"Freelancer",
+    }
+}
 
-// const {salary,name} =person;
-// console.log(name, salary);
+// const {salary,name, job:{job1}} =person;
+// console.log(name, salary, job1);
+
+const country = [ "Bangladesh", "France", "Germany", "United States", "Japan", "Arabia"];
+const [country1, country2,  country3, ...rest ] = country;
+console.log(rest  )    
 
 const friends = ['salman','amir','sohel'];
 const [name, ...allName] = friends;
-console.log(name, allName)
+const mixArr = [...country, ...friends];
+// console.log(name, allName)
+// console.log(mixArr)
